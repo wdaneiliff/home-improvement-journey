260519af4f7a08bd1b29ffcdf97e1a8a0b728eeb
Mike Nikles <mnikles@mobify.com>
2016-10-02 10:58:15 -0700

    Render name in app.

--- src/app/app.js
+++ src/app/app.js
@@ -1,32 +1,34 @@
 /* @flow */
-import React, { Component } from 'react';
-import logo from '../logo.svg';
-import './app.css';
+import React, { Component } from 'react'
+import logo from '../logo.svg'
+import './app.css'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.name = 'Mike'
-  }
-
-  myTest = () => {
-    console.log(this.name)
-  }
-
   render() {
-    this.myTest()
+    const {
+      name
+    } = this.props
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React
+          {
+            name && <span>, {name}</span>
+          }
+          </h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+App.defaultProps = {
+  name: ''
+}
+
+export default App
