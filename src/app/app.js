dfbcaf8aa20dbff0a4875f30664dece16848f997
Mike Nikles <mnikles@mobify.com>
2016-10-08 13:29:08 -0700

    Add SASS example.

--- src/app/app.js
+++ src/app/app.js
@@ -34,6 +34,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <span className="with-sass">I am styled with SASS</span>
       </div>
     )
   }
