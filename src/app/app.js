7920f059bc4f5837940f5bdf5fd6f297b1088042
Mike Nikles <mnikles@mobify.com>
2016-10-16 16:21:57 -0700

    Add basic implementation of the React Router v4 alpha.

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,34 @@
 import React, { Component, PropTypes } from 'react'
+import { BrowserRouter, Match, Miss, Link } from 'react-router'
 import logo from '../logo.svg'
 import './app.css'
 // import { loadProduct } from '../utils/bd-api'
+
+const Home = () => (
+  <div>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+    <span className="with-sass">I am styled with SASS</span>
+  </div>
+)
+
+const About = (props) => {
+  console.log(props)
+  return (
+    <h1>About</h1>
+  )
+}
+
+const NoMatch = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Sorry but didn’t match any pages</p>
+    </div>
+  )
+}
 
 class App extends Component {
   constructor(props) {
@@ -21,20 +48,23 @@ class App extends Component {
     // console.log(this.state.product)
 
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React
-          {
-            name && <span>, {name}</span>
-          }
-          </h2>
+      <BrowserRouter>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React
+            {
+              name && <span>, {name}</span>
+            }
+            </h2>
+          </div>
+          <Link to="/">Home</Link><span> | </span>
+          <Link to="/about">About</Link>
+          <Match exactly pattern="/" component={Home} />
+          <Match pattern="/about" component={About} />
+          <Miss component={NoMatch}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span className="with-sass">I am styled with SASS</span>
-      </div>
+      </BrowserRouter>
     )
   }
 }
